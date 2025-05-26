e8382deb4d9a48f7cbf1915dd113e0138e9dad10
Mariam Raouf <105318815+MariamRaouf30@users.noreply.github.com>
2025-05-26 16:26:20 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/dataUsage", methods=["GET"])
+def data_usage():
+    user_id = request.args.get("user_id")
+ 
+    return jsonify({
+        "user_id": user_id,
+        "plan": "Monthly 10GB",
+        "total_data_mb": 10240,
+        "used_data_mb": 7345,
+        "remaining_data_mb": 2895,
+        "renewal_date": "2025-06-01"
+    }), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
